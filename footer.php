ed68292872a995090c41af132bc7432cd7a99a68
Lærke Laulund <139771208+LLaulund@users.noreply.github.com>
2023-08-07 15:41:28 +0200

    Update footer.php

---
+++ footer.php
@@ -0,0 +1,14 @@
+<article>
+  <h5><i>This website is an exam project at the University of Southern Denmark </i></h5>
+</article>
+<section>
+  <!-- Code for SoME icons retrieved from: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_social_media_buttons2-->
+  <p>Follow us on Social Media</p>
+  <a href="#" class="fa fa-facebook"></a>
+  <a href="#" class="fa fa-twitter"></a>
+  <a href="#" class="fa fa-instagram"></a>
+  <a href="#" class="fa fa-youtube"></a>
+</section>
+<section>
+  <a href="login.php">Admin Login</a>
+</section>
