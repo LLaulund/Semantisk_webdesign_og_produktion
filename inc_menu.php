c25a597f7d16a0afbfc8b34696b6c4a4e569b29b
Lærke Laulund <139771208+LLaulund@users.noreply.github.com>
2023-08-07 15:41:56 +0200

    Update inc_menu.php

---
+++ inc_menu.php
@@ -0,0 +1,10 @@
+<div class="topnav" id="myTopnav">
+  <a href="index.php" class="active">Forside</a>
+  <a href="mw_personer.php">Musikere og Bands</a>
+  <a href="mw_musicrecordings.php">Musik</a>
+  <a href="mw_musicalbums.php">Albummer</a>
+  <a href="mw_information.php">Information</a>
+  <a href="javascript:void(0);" class="burger" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+</div>
