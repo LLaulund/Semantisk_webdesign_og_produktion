cce4c45e556dd5d5a24912784eaab0628354dab7
Lærke Laulund <139771208+LLaulund@users.noreply.github.com>
2023-08-07 15:39:17 +0200

    Update adm_inc_menu.php

---
+++ adm_inc_menu.php
@@ -0,0 +1,10 @@
+<div class="topnav" id="myTopnav">
+  <a href="adm_index.php">Admin forside</a>
+  <a href ="velkom.php"> Min side</a>
+  <a href ="index.php"> MusicWorld forside</a>
+  <a href="adm_add_content.php" id="add_content">Tilføj data</a>
+  <a href ="logout.php"> Log af</a>
+  <a href="javascript:void(0);" class="burger" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+</div>
