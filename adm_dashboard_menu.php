355b8d524ef1f9ed821c30cea533c9b5d2d97354
Lærke Laulund <139771208+LLaulund@users.noreply.github.com>
2023-08-07 15:38:56 +0200

    Update adm_dashboard_menu.php

---
+++ adm_dashboard_menu.php
@@ -0,0 +1,10 @@
+  <!-- Sidebar, code adjusted from: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_sidenav_fixed  -->
+  <div class="sidenav">
+    <a href="mw_adm_add_place.php">Sted</a>
+    <a href="mw_adm_add_person.php">Kunstner</a>
+    <a href="mw_adm_add_musicrecording.php">Musikstykker</a>
+    <a href="mw_adm_add_musicalbum.php">Musikalbum</a>
+    <a href="mw_adm_add_musicgroup.php">Musikgruppe</a>
+    <a href="mw_adm_add_organization.php">Pladeselsskab</a>
+    <a href="mw_adm_add_musicalbum_has_musicgroup.php">Musikgruppe har album</a>
+  </div>
